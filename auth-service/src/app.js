e8eb6a2af1667f5e7332a79ada62cd4974f8e4e6
pabloobitw <pabloobitiu@gmail.com>
2026-04-30 12:54:12 -0600

    feat: add Swagger documentation for auth endpoints

--- auth-service/src/app.js
+++ auth-service/src/app.js
@@ -3,6 +3,35 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const authRoutes = require("./routes/auth.routes.js");
 
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'API - Reviews',
+      version: '1.0.0',
+      description: 'Reseñas de restaurantes',
+    },
+    servers: [
+      { url: 'http://localhost:3000' }
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        },
+      },
+    },
+  },
+  apis: ['./src/routes/*.js'],
+};
+
+
+const swaggerSpec = swaggerJsdoc(swaggerOptions);
 
 dotenv.config();
 
@@ -14,5 +43,9 @@ app.use(express.json());
 
 
 app.use("/api/auth", authRoutes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+
+
 
 module.exports = app;
--- auth-service/src/routes/auth.routes.js
+++ auth-service/src/routes/auth.routes.js
@@ -4,8 +4,90 @@ const { register, login, healthCheck, registerAdmin } = require("../controllers/
 const router = express.Router();
 
 router.get("/health", healthCheck);
+/**
+ * @openapi
+ * /api/auth/register:
+ *   post:
+ *     summary: Registro de un nuevo usuario no ADMIN
+ *     tags:
+ *       - Servicio de auth
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 example: "pablocorreobonito14@gmail.com"
+ *               password:
+ *                 type: string
+ *                 example: "123456!"
+ *     responses:
+ *       201:
+ *         description: Happy path
+ *       400:
+ *         description: Error
+ */
 router.post("/register", register);
+/**
+ * @openapi
+ * /api/auth/register/admin:
+ *   post:
+ *     summary: Registro de un nuevo ADMIN
+ *     tags:
+ *       - Servicio de auth
+ *     security: 
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 example: "adminbonito14@gmail.com"
+ *               password:
+ *                 type: string
+ *                 example: "123456aA!"
+ *     responses:
+ *       201:
+ *         description: Usuario registrado correctamente
+ *       400:
+ *         description: Datos invalidos
+ *       403: 
+ *         description: Acceso denegado
+ */
 router.post("/register/admin", registerAdmin);
+/**
+ * @openapi
+ * /api/auth/login:
+ *   post:
+ *     summary: Login de un usuario ya registrado
+ *     tags:
+ *       - Servicio de auth
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 example: "pablocorreobonito14@gmail.com"
+ *               password:
+ *                 type: string
+ *                 example: "123456!"
+ *     responses:
+ *       200:
+ *         description: Incio de sesión correctamente
+ *       400:
+ *         description: Credenciales incorrectas 
+ */
 router.post("/login", login);
 
 module.exports = router;
