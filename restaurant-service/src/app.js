74fd6096b39329c8ed7c760ea59cc0f12f6dc99e
pablobitw <pabloobitiu@gmail.com>
2026-05-01 21:27:05 -0600

    feat: add Swagger documentation for restaurant endpoints

--- restaurant-service/src/app.js
+++ restaurant-service/src/app.js
@@ -1,10 +1,31 @@
 const express = require('express');
 const cors = require('cors');
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 const restaurantRoutes = require('./routes/restaurant.routes');
+
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'API - Restaurants',
+      version: '1.0.0',
+      description: 'Servicio público de catálogo de restaurantes',
+    },
+    servers: [
+      { url: 'http://localhost:3003' } 
+    ],
+  },
+  apis: ['./src/routes/*.js'],
+};
+
+const swaggerSpec = swaggerJsdoc(swaggerOptions);
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
 app.use('/api/restaurants', restaurantRoutes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 module.exports = app;
